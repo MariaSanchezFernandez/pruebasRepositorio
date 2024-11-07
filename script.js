bea7cdcd26f5050aa79895367a86c47a55d8defe
Maria Sanchez Fernandez <maria.sanchez@escuelaestech.es>
2024-11-07 21:23:06 +0100

    Boton hecho

--- script.js
+++ script.js
@@ -1,95 +1,102 @@
-        gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger);
 
-        //Comandos para que al bajar, cambie el fondo de color
 
-        const locoScroll = new LocomotiveScroll({
-            el: document.querySelector(".container"),
-            smooth: true
-        });
+        //Comandos para que al bajar, cambie el fondo de color entre blanco y negro por distintas
 
-        ScrollTrigger.scrollerProxy(".container", {
-            scrollTop(value) {
-                return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-            },
-            getBoundingClientRect() {
-                return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-            },
-            pinType: document.querySelector(".container").style.transform ? "transform" : "fixed"
-        });
+    const isMobile = window.innerWidth <= 768; // Definir que el tamaño máximo para móviles es 768px
 
-        gsap.to("body", {
-            "--color": "white",
-            immediateRender: false,
-            scrollTrigger: {
-                trigger: ".inicio",
-                scroller: ".container",
-                scrub: true,
-                start: "top bottom",
-                end: "top top"
-            }
-        });
+if (!isMobile) {
+  
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".container"),
+        smooth: true
+    });
 
-        gsap.to("body", {
-            "--color": "white",
-            immediateRender: false,
-            scrollTrigger: {
-                trigger: ".presentacion",
-                scroller: ".container",
-                scrub: true,
-                start: "top bottom",
-                end: "top top"
-            }
-        });
+    ScrollTrigger.scrollerProxy(".container", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+        },
+        pinType: document.querySelector(".container").style.transform ? "transform" : "fixed"
+    });
 
-        gsap.to("body", {
-            "--color": "black",
-            immediateRender: false,
-            scrollTrigger: {
-                trigger: ".cv",
-                scroller: ".container",
-                scrub: true,
-                start: "top bottom",
-                end: "top top"
-            }
-        });
-
-        gsap.to("body", {
-            "--color": "white",
-            immediateRender: false,
-            scrollTrigger: {
-                trigger: ".proyectos",
-                scroller: ".container",
-                scrub: true,
-                start: "top bottom",
-                end: "top top"
-            }
-        });
-
-        gsap.to("body", {
-            "--color": "black",
-            immediateRender: false,
-            scrollTrigger: {
-                trigger: ".formulario",
-                scroller: ".container",
-                scrub: true,
-                start: "top bottom",
-                end: "top top"
-            }
-        });
-
-        locoScroll.on("scroll", ScrollTrigger.update);
-        ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-        ScrollTrigger.refresh();
-
-        //Botón del cv fijo
-
-        ScrollTrigger.create({
-            trigger: ".botonWrapper",
+    gsap.to("body", {
+        "--color": "white",
+        immediateRender: false,
+        scrollTrigger: {
+            trigger: ".inicio",
             scroller: ".container",
-            pin: true,
-            //pinSpacing:false,
-            start: "center center",
-            end: "+=1231",
-            markers: false
-          });
+            scrub: true,
+            start: "top bottom",
+            end: "top top"
+        }
+    });
+
+    gsap.to("body", {
+        "--color": "white",
+        immediateRender: false,
+        scrollTrigger: {
+            trigger: ".presentacion",
+            scroller: ".container",
+            scrub: true,
+            start: "top bottom",
+            end: "top top"
+        }
+    });
+
+    gsap.to("body", {
+        "--color": "black",
+        immediateRender: false,
+        scrollTrigger: {
+            trigger: ".cv",
+            scroller: ".container",
+            scrub: true,
+            start: "top bottom",
+            end: "top top"
+        }
+    });
+
+    gsap.to("body", {
+        "--color": "white",
+        immediateRender: false,
+        scrollTrigger: {
+            trigger: ".proyectos",
+            scroller: ".container",
+            scrub: true,
+            start: "top bottom",
+            end: "top top"
+        }
+    });
+
+    gsap.to("body", {
+        "--color": "black",
+        immediateRender: false,
+        scrollTrigger: {
+            trigger: ".formulario",
+            scroller: ".container",
+            scrub: true,
+            start: "top bottom",
+            end: "top top"
+        }
+    });
+
+    locoScroll.on("scroll", ScrollTrigger.update);
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+    //Botón del cv fijo
+
+    ScrollTrigger.create({
+        trigger: ".botonWrapper",
+        scroller: ".container",
+        pin: true,
+        //pinSpacing:false,
+        start: "center center",
+        end :isMobile ? "+=500" : "+=1231",
+        markers: false
+        });
+
+}
